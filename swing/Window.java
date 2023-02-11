a3addf5ced8f18ae689d27f59edebfacede97b30
alafSantos <alaf.ns4@gmail.com>
2023-02-11 19:34:27 +0100

    Client OK

--- swing/Window.java
+++ swing/Window.java
@@ -10,6 +10,9 @@ import javax.swing.JToolBar;
 import javax.swing.JMenu;
 import javax.swing.JTextField;
 
+import java.io.*;
+import java.net.*;
+
 public class Window extends JFrame implements ActionListener {
   private static final long serialVersionUID = 1L;
   private JTextArea textArea;
@@ -65,24 +68,114 @@ public class Window extends JFrame implements ActionListener {
     setVisible(true);
   }
 
+  static final String DEFAULT_HOST = "localhost";
+  static final int DEFAULT_PORT = 3331;
+
+  static String host = DEFAULT_HOST;
+  static int port = DEFAULT_PORT;
+
+  static Client client = null;
+
+  public static void main(String argv[]) {
+    if (argv.length >= 1)
+      host = argv[0];
+    if (argv.length >= 2)
+      port = Integer.parseInt(argv[1]);
+
+    try {
+      client = new Client(host, port);
+    } catch (Exception excp) {
+      System.err.println("Client: Couldn't connect to " + host + ":" + port);
+      System.exit(1);
+    }
+    new Window();
+  }
+
+  // -------------------------CLIENT.JAVA-------------------------
+
+  private static class Client {
+
+    private Socket sock;
+    private BufferedReader input;
+    private BufferedWriter output;
+
+    ///
+    /// Initialise la connexion.
+    /// Renvoie une exception en cas d'erreur.
+    ///
+    public Client(String host, int port) throws UnknownHostException, IOException {
+      try {
+        sock = new java.net.Socket(host, port);
+      } catch (java.net.UnknownHostException e) {
+        System.err.println("Client: Couldn't find host " + host + ":" + port);
+        throw e;
+      } catch (java.io.IOException e) {
+        System.err.println("Client: Couldn't reach host " + host + ":" + port);
+        throw e;
+      }
+
+      try {
+        input = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        output = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
+      } catch (java.io.IOException e) {
+        System.err.println("Client: Couldn't open input or output streams");
+        throw e;
+      }
+    }
+
+    ///
+    /// Envoie une requete au server et retourne sa reponse.
+    /// Noter que la methode bloque si le serveur ne repond pas.
+    ///
+    public String send(String request) {
+      // Envoyer la requete au serveur
+      try {
+        request += "\n"; // ajouter le separateur de lignes
+        output.write(request, 0, request.length());
+        output.flush();
+      } catch (java.io.IOException e) {
+        System.err.println("Client: Couldn't send message: " + e);
+        return null;
+      }
+
+      // Recuperer le resultat envoye par le serveur
+      try {
+        return input.readLine();
+      } catch (java.io.IOException e) {
+        System.err.println("Client: Couldn't receive message: " + e);
+        return null;
+      }
+    }
+  }
+  // --------------------------------------------------------
+
   public void actionPerformed(ActionEvent e) {
+
+    System.out.println("Client connected to " + host + ":" + port);
 
     if (e.getSource() == btnClear) {
       textArea.setText("");
+      textField.setText("");
     } else if (e.getSource() == btnExit) {
       System.exit(0);
     } else {
       String txt = textField.getText();
+      String command = null;
       if (e.getSource() == button1) {
-        textArea.append(txt + " play\n");
+        command = "play " + txt;
+
       } else if (e.getSource() == button2) {
-        textArea.append(txt + " show\n");
+        command = "find " + txt;
+      }
+      System.out.print("Request: " + command + "\n");
+      try {
+        String response = client.send(command);
+        System.out.println("Response: " + response + "\n");
+        textArea.append(response+"\n");
+      } catch (Exception excp) {
+        System.err.println("Client: error");
+        return;
       }
     }
-
-  }
-
-  public static void main(String[] args) {
-    new Window();
   }
 }
